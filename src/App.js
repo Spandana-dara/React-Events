a03df4399239ce3c0bff242f5c1bd20573ef60da
Spandana <spandana1286@gmail.com>
2024-04-15 17:34:10 -0400

    added images

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import { useState } from "react";
+import ShowAnimals from "./ShowAnimals";
+
+function getRandomAnimal() {
+  const animals = ["bird", "cat", "cow", "dog", "gator", "horse"];
+
+  return animals[Math.floor(Math.random() * animals.length)];
+}
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [animals, setAnimals] = useState([]);
+
   const handleClick = () => {
-    setCount(count + 1);
+    setAnimals([...animals, getRandomAnimal()]);
   };
+
+  const renderedAnimals = animals.map((animal, index) => {
+    return <ShowAnimals type={animal} key={index} />;
+  });
+
   return (
     <div>
-      <button onClick={handleClick}>Add animal</button>
-      <div>Number of Animals : {count}</div>
+      <button onClick={handleClick}>Add Animal</button>
+      <div>{renderedAnimals}</div>
     </div>
   );
 }
--- src/ShowAnimals.js
+++ src/ShowAnimals.js
@@ -1,5 +1,34 @@
-function ShowAnimals() {
-  return <div>Show!</div>;
+import { useState } from "react";
+
+import bird from "./svg/bird.svg";
+import cat from "./svg/cat.svg";
+import cow from "./svg/cow.svg";
+import dog from "./svg/dog.svg";
+import gator from "./svg/gator.svg";
+import heart from "./svg/heart.svg";
+import horse from "./svg/horse.svg";
+
+const svgMap = {
+  bird,
+  cat,
+  cow,
+  dog,
+  gator,
+  horse,
+};
+
+function ShowAnimals({ type }) {
+  const [clicks, setClicks] = useState(0);
+  const handleClick = () => {
+    setClicks(clicks + 1);
+  };
+
+  return (
+    <div onClick={handleClick}>
+      <img alt="animal" src={svgMap[type]} />
+      <img src={heart} alt="horse" style={{ width: 10 + 10 * clicks }} />
+    </div>
+  );
 }
 
 export default ShowAnimals;
